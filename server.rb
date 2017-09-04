6da423c05d398e4e94b72ac6dbdfb715a83839bd
Cong Cong Bo <cong@hippocampus.ai>
2017-09-04 12:43:50 +0100

    server stores query string as key and value

--- server.rb
+++ server.rb
@@ -1,18 +1,26 @@
 require 'socket'
 
 app = Proc.new do
-  ['200', {'Content-Type' => 'text/html'}, ["Hello!, The time is #{Time.now}"]]
+  ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
 end
 
-server = TCPServer.new 2345
+server = TCPServer.new 5678
 
 while session = server.accept
   request = session.gets
   puts request
 
+  method, full_path = request.split(" ")
+  path, query = full_path.split("?")
+  key, value = query.split("=")
+
+  puts key
+  puts value
+
   status, headers, body = app.call({})
 
   session.print "HTTP/1.1 #{status}\r\n"
+
   headers.each do |key, value|
     session.print "#{key}: #{value}\r\n"
   end
